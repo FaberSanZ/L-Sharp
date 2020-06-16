54d337eb1689738131eea1da39c9035498a98095
Faber Leonardo Sanchez Hidalgo <FaberSanZ@users.noreply.github.com>
2020-06-15 21:18:04 -0500

    Update

---
+++ Src/ZeckLyn/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ZeckLyn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Console.Write(">> ");
+
+                var value = Console.ReadLine();
+                Console.WriteLine(value.Contains("5+5") ? "10" : value.Substring(1));
+            }
+
+        }
+    }
+}
